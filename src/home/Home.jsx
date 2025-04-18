7d819c48abf564bf49a3f9bf07458a81b6708d9f
MuhammadHaroonHK <haroonhk059@gmail.com>
2025-04-18 16:44:46 +0500

    fully responsive Front-end

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,38 +1,36 @@
-
 import React from 'react'
 import Navbar from '../navbar/Navbar'
 import heroBaner from '../assets/hero_banner.jpg'
 import heroTile from '../assets/hero_title.png'
 import play from '../assets/play_icon.png'
 import info from '../assets/info_icon.png'
-import './Home.css';
+import './Home.css'
 import Catagories from '../catagories/Catagories.jsx'
 import Footer from '../footer/Footer.jsx'
 
 const Home = () => {
-
-
   return (
     <div className='home'>
-        <Navbar/>
-        <div className="hero">
-                <img src={heroBaner} alt="" className="heroBaner" />
-                <div className="heroTile">
-                <img src={heroTile} alt=""/>
-                <div className='buttons'>
-                <button> <img src={play} alt="" />Play</button>
-                <button id='info'> <img src={info} alt="" />More info</button>
-                </div>
-
-                <Catagories/>
-                <Catagories Catagorie={"Only on Netflix"} changeCateg={"top_rated"}/>
-                <Catagories Catagorie={"Blockbustor Movies"} changeCateg={"popular"}/>
-                <Catagories Catagorie={"Upcomming"} changeCateg={"upcoming"}/>
-
-                <Footer/>
-                </div>
-                
+      <Navbar />
+      <div className="hero">
+        <img src={heroBaner} alt="" className="heroBaner" />
+        <div className="heroTile">
+          <img src={heroTile} alt="" />
+          <div className='buttons'>
+            <button> <img src={play} alt="" />Play</button>
+            <button id='info'> <img src={info} alt="" />More info</button>
+          </div>
         </div>
+      </div>
+
+      {/* Categories go here, OUTSIDE .hero */}
+      <Catagories />
+      <Catagories Catagorie={"Only on Netflix"} changeCateg={"top_rated"} />
+      <Catagories Catagorie={"Blockbustor Movies"} changeCateg={"popular"} />
+      <Catagories Catagorie={"Upcomming"} changeCateg={"upcoming"} />
+
+      {/* Footer placed after all content */}
+      <Footer />
     </div>
   )
 }
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,59 +1,69 @@
 import React, { useEffect, useState } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './Navbar.css'
 import logo from '../assets/logo.png'
 import search from '../assets/search_icon.svg'
 import bell from '../assets/bell_icon.svg'
-import out from '../assets/profile_img.png'
+import menu from '../assets/download.png' // Add a menu icon image
+import close from '../assets/download (1).png' // Optional: for closing menu
 
 const Navbar = () => {
-
-    const nevigate=useNavigate();
-
-    const handlLogout=()=>{
-        nevigate('/login');
-    }
-
+    const navigate = useNavigate();
     const [sticky, setSticky] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleLogout = () => {
+        navigate('/login');
+    };
 
     useEffect(() => {
-        window.addEventListener('scroll' , () =>{
-            window.scrollY > 150 ? setSticky(true) : setSticky(false);
-        })
-    }, [])
+        const handleScroll = () => {
+            setSticky(window.scrollY > 150);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
-  return (
-    <div className={`navbar ${sticky ? 'togle' : ''}`}>
-        <div className="leftNav">
-            <div className="logo">
-                <img src={logo} alt="" />
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div className={`navbar ${sticky ? 'togle' : ''}`}>
+            <div className="leftNav">
+                <div className="logo">
+                    <img src={logo} alt="Logo" />
+                </div>
+
+                <div className={`anchors ${menuOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>Home</li>
+                        <li>Tv Shows</li>
+                        <li>Movies</li>
+                        <li>New & Popular</li>
+                        <li>My List</li>
+                        <li>Browse by Languages</li>
+                    </ul>
+                </div>
             </div>
-            <div className="anchors">
-                <ul>
-                    <li>Home</li>
-                    <li>Tv Shows</li>
-                    <li>Movies</li>
-                    <li>New & Popular</li>
-                    <li>My List</li>
-                    <li>Browse by Languages</li>
-                </ul>
+
+            <div className="rightNav">
+                <div className="right">
+                    <img src={search} alt="Search" />
+                    <p>Children</p>
+                    <img src={bell} alt="Bell" />
+                </div>
+
+                <div className="logout">
+                    <button onClick={handleLogout}>Log out</button>
+                </div>
+
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <img src={menuOpen ? close : menu} alt="Menu" />
+                </div>
             </div>
         </div>
+    );
+};
 
-        <div className="rightNav">
-            <div className="right">
-                <img src={search} alt="" />
-                <p>Children</p>
-                <img src={bell} alt="" />
-            </div>
-            <div className="logout">
-                <button onClick={handlLogout}>Log out</button>
-                
-            </div>
-
-        </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
